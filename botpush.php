8497339548aca7744e5c39a2b877b30634478620
paramee <aepsrhunter@gmail.com>
2019-04-24 20:49:48 +0700

    แแแ

--- botpush.php
+++ botpush.php
@@ -8,12 +8,13 @@ $access_token = '2xrbcTrUtlgTgJWoFAitjR/v3wiyK2yWTP++h0vDajTc7wS9o2a5BFvFcczIei2
 
 $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
-$pushID = 'Ue12fab23d245e4c56d678be6e55a33f1';
+// $pushID = 'Ue12fab23d245e4c56d678be6e55a33f1';
+$pushID = 'U826ec160cae46b8fc9f06fe676c2510f';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีครับ');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo "สวัสดีครับ";
